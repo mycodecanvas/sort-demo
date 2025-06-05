edcabd5655d358589de9263f5956eae3f053527e
thuc_hi_tran <thuchitran@gmail.com>
2025-06-06 02:22:58 +1000

    set up project

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import Table from "./components/Table";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+type ToDoItem = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+export type ToDoList = ToDoItem[];
+
+function App() {
+  const [data, setData] = useState<ToDoList>([]);
+  const [sortedData, setSortedData] = useState<ToDoList>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setSortedData(json);
+      });
+  }, []);
+
+  const sortHandler = (value: "userId" | "title") => {
+    setSortedData(sortData(value, data));
+  };
+
+  const sortData = (value: "userId" | "title", data: ToDoList) => {
+    console.log("sort function called!");
+    const sortedData = [...data].sort((a, b) => {
+      if (value === "userId") {
+        return a.userId - b.userId;
+      } else if (value === "title") {
+        return a.title > b.title ? 1 : -1;
+      }
+      return 0;
+    });
+    return sortedData;
+  };
+
+  return (
+    <div>
+      <h1 className="text-center">To Do List</h1>
+      <div className="button-wrapper">
+        <button onClick={() => sortHandler("userId")}>Sort By User ID</button>
+        <button onClick={() => sortHandler("title")}>Sort By Title</button>
+      </div>
+
+      <Table headers={["User ID", "ID", "Title"]} data={sortedData} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState, type ReactNode } from "react";
+import type { ToDoList } from "../App";
+
+type TableProps = {
+  headers: string[];
+  data: ToDoList;
+};
+
+function Table({ headers, data }: TableProps) {
+  console.log("Table component rendered");
+  const [mappedData, setMappedData] = useState<unknown[][]>([]);
+  useEffect(() => {
+    if (data.length > 0) {
+      const mappedData = data.map((row) => Object.values(row));
+      setMappedData(mappedData);
+    }
+  }, [data]);
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {mappedData.map((row, index) => (
+          <tr key={index}>
+            {row.map(
+              (cell, cellIndex) =>
+                cellIndex < row.length - 1 && (
+                  <td key={cellIndex}>{cell as ReactNode}</td>
+                )
+            )}
+          </tr>
+        ))}
+        {mappedData.length === 0 && (
+          <tr>
+            <td colSpan={headers.length}>No data available</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
